938ffa1e59a4a208b97c4b5713523625335860e9
Kieran Llarena <baymax709@gmail.com>
2023-12-11 11:11:47 -0500

    refactor: created mobile navbar svelte store

--- src/lib/mobile/Navbar.svelte
+++ src/lib/mobile/Navbar.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
-  export let isToggled = false
+	import { mobileDropdownEnabled } from "$lib/state";
+
+  let isToggled: boolean
+  mobileDropdownEnabled.subscribe(val => {
+    isToggled = val
+  })
+
+  const toggleNavbar = () => {
+    mobileDropdownEnabled.update(currVal => currVal = !currVal)
+  }
 </script>
 
 <header>
   <nav>
-    <button on:click={() => isToggled = !isToggled}>
+    <button on:click={toggleNavbar}>
       {#if !isToggled}
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import NavDropdown from "$lib/mobile/NavDropdown.svelte";
   import MobileNavbar from "$lib/mobile/Navbar.svelte";
+	import { mobileDropdownEnabled } from "$lib/state";
 
-  let mobileNavbarIsToggled: boolean
+  let mNavbarIsToggled: boolean
+  mobileDropdownEnabled.subscribe(val => {
+    mNavbarIsToggled = val
+  })
 </script>
 
-<MobileNavbar bind:isToggled={mobileNavbarIsToggled}  />
-{#if !mobileNavbarIsToggled}
+<MobileNavbar />
+{#if !mNavbarIsToggled}
   <slot />
 {:else}
   <NavDropdown />
