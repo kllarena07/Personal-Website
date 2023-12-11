5bdb1ca4a5c1c69ff4161742dd6b7f603fbb08be
Kieran Llarena <baymax709@gmail.com>
2023-12-11 17:41:48 -0500

    chore: added neuroscan ai project

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,6 +1,7 @@
 import SumSum from '$lib/thumbnails/sumsum.avif';
 import ADHDMag from '$lib/thumbnails/adhd.jpg';
 import CriCri from '$lib/thumbnails/cricri.svg';
+import NeuroScanAI from '$lib/thumbnails/neuroscanai.jpg';
 
 import React from '$lib/tech/react.svg';
 import Tailwind from '$lib/tech/tailwind.svg';
@@ -10,6 +11,8 @@ import Vercel from '$lib/tech/vercel.svg';
 import Typescript from '$lib/tech/typescript.svg';
 import Pocketbase from '$lib/tech/pocketbase.svg';
 import CSS from '$lib/tech/css.svg';
+import Python from '$lib/tech/python.svg';
+import Figma from '$lib/tech/figma.svg';
 
 export const Projects = [
 	{
@@ -32,5 +35,12 @@ export const Projects = [
 		desc: 'Personal website for the Detroit musician, El Cri Cri',
 		thumbnail: CriCri,
 		tech: [Svelte, Vercel, Javascript, CSS]
+	},
+	{
+		name: 'NeuroScan AI',
+		link: 'https://github.com/kllarena07/Brain-Tumor-Detector',
+		desc: 'Web app that detects brain tumors in an inputted MRI scan',
+		thumbnail: NeuroScanAI,
+		tech: [Svelte, Vercel, Javascript, Python, Figma]
 	}
 ];
