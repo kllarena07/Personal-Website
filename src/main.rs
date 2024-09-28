6674bb65d1446be31e1e3124c2b8784eafe9de85
Kieran Llarena <baymax709@gmail.com>
2024-09-28 12:05:03 -0400

    feat: skills page

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,17 @@ async fn projects() -> Result<NamedFile> {
     Ok(NamedFile::open(path)?)
 }
 
+#[get("/skills")]
+async fn skills() -> Result<NamedFile> {
+    let root = env::current_dir().unwrap();
+    let abs_path = format!("{}/app/skills.html", root.to_str().unwrap());
+    let path: PathBuf = PathBuf::from(abs_path);
+    Ok(NamedFile::open(path)?)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(projects))
+    HttpServer::new(|| App::new().service(index).service(projects).service(skills))
         .bind("127.0.0.1:8080")? // Bind the server to localhost on port 8080
         .run()
         .await
